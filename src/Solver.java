a5d155393bd1a15fe575d1aca9638f59063b8207
Zirui Li <l.zirui19@gmail.com>
2024-03-08 17:27:14 +1100

    [Algorithm] update addBridge and removeBridge

--- src/Solver.java
+++ src/Solver.java
@@ -1,61 +1,61 @@
-package src;
+// package src;
 
-import java.util.List;
+// import java.util.List;
 
-import src.entities.Entity;
-import src.entities.Island;
+// import src.entities.Entity;
+// import src.entities.Island;
 
-public class Solver {
-    private Entity[][] gameMap;
+// public class Solver {
+//     private Entity[][] gameMap;
 
-    private List<Island> islands;
+//     private List<Island> islands;
 
-    public Solver(Entity[][] gameMap) {
-        this.gameMap = gameMap;
-    }
+//     public Solver(Entity[][] gameMap) {
+//         this.gameMap = gameMap;
+//     }
 
-    public boolean solve() {
-        return solve(0);
-    }
+//     public boolean solve() {
+//         return solve(0);
+//     }
 
-    private boolean solve(int islandIndex) {
-        if (islandIndex == islands.size()) {
-            return allIslandsSatisfied();
-        }
+//     private boolean solve(int islandIndex) {
+//         if (islandIndex == islands.size()) {
+//             return allIslandsSatisfied();
+//         }
 
-        Island currentIsland = islands.get(islandIndex);
-        if (currentIsland.getIslandValue() == 0) {
-            return solve(islandIndex + 1);
-        }
+//         Island currentIsland = islands.get(islandIndex);
+//         if (currentIsland.getIslandValue() == 0) {
+//             return solve(islandIndex + 1);
+//         }
 
-        for (Island targetIsland : currentIsland.getPotentialIslands()) {
-            for (int bridgeCount = 1; bridgeCount <= 3; bridgeCount++) {
-                if (canPlaceBridges(currentIsland, targetIsland, bridgeCount)) {
-                    gameMap.addBridge(currentIsland.getXCord(), currentIsland.getYCord(), targetIsland.getXCord(), targetIsland.getYCord(), bridgeCount);
-                    currentIsland.setIslandValue(currentIsland.getIslandValue() - bridgeCount);
-                    targetIsland.setIslandValue(targetIsland.getIslandValue() - bridgeCount);
+//         for (Island targetIsland : currentIsland.getPotentialIslands()) {
+//             for (int bridgeCount = 1; bridgeCount <= 3; bridgeCount++) {
+//                 if (canPlaceBridges(currentIsland, targetIsland, bridgeCount)) {
+//                     gameMap.addBridge(currentIsland.getXCord(), currentIsland.getYCord(), targetIsland.getXCord(), targetIsland.getYCord(), bridgeCount);
+//                     currentIsland.setIslandValue(currentIsland.getIslandValue() - bridgeCount);
+//                     targetIsland.setIslandValue(targetIsland.getIslandValue() - bridgeCount);
 
-                    if (solve(islandIndex + 1)) {
-                        return true;
-                    }
-                    removeBridges(currentIsland, targetIsland, bridgeCount);
-                    currentIsland.setIslandValue(currentIsland.getIslandValue() + bridgeCount);
-                    targetIsland.setIslandValue(targetIsland.getIslandValue() + bridgeCount);
-                }
-            }
-        }
-        return false;
-    }
+//                     if (solve(islandIndex + 1)) {
+//                         return true;
+//                     }
+//                     removeBridges(currentIsland, targetIsland, bridgeCount);
+//                     currentIsland.setIslandValue(currentIsland.getIslandValue() + bridgeCount);
+//                     targetIsland.setIslandValue(targetIsland.getIslandValue() + bridgeCount);
+//                 }
+//             }
+//         }
+//         return false;
+//     }
 
 
 
-    private boolean allIslandsSatisfied() {
-        for (Island island : islands) {
-            if (island.getIslandValue() != 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+//     private boolean allIslandsSatisfied() {
+//         for (Island island : islands) {
+//             if (island.getIslandValue() != 0) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
 
-}
+// }
--- src/map/GameMap.java
+++ src/map/GameMap.java
@@ -39,13 +39,25 @@ public class GameMap {
     }
 
     public void addBridge(int row1, int col1, int row2, int col2, int planks, boolean isHorizontal) {
-        int startCol = Math.min(col1, col2);
-        int startRow = Math.min(row1, row2);
-        int endCol = Math.max(col1, col2);
-        int endRow = Math.max(row1, row2);
+        int startCol = isHorizontal ? Math.min(col1, col2) + 1 : Math.min(col1, col2);
+        int startRow = isHorizontal ? Math.min(row1, row2) : Math.min(row1, row2) + 1;
+        int endCol = isHorizontal ? Math.max(col1, col2) - 1 : Math.max(col1, col2);
+        int endRow = isHorizontal ? Math.max(row1, row2) : Math.max(row1, row2) - 1;
         for (int row = startRow; row <= endRow; row++) {
             for (int col = startCol; col <= endCol; col++) {
-                gameMap[startRow][startCol] = new Bridge(startRow, startCol, planks, isHorizontal);
+                gameMap[row][col] = new Bridge(row, col, planks, isHorizontal);
+            }
+        }
+    }
+
+    public void removeBridge(int row1, int col1, int row2, int col2, boolean isHorizontal) {
+        int startCol = isHorizontal ? Math.min(col1, col2) + 1 : Math.min(col1, col2);
+        int startRow = isHorizontal ? Math.min(row1, row2) : Math.min(row1, row2) + 1;
+        int endCol = isHorizontal ? Math.max(col1, col2) - 1 : Math.max(col1, col2);
+        int endRow = isHorizontal ? Math.max(row1, row2) : Math.max(row1, row2) - 1;
+        for (int row = startRow; row <= endRow; row++) {
+            for (int col = startCol; col <= endCol; col++) {
+                gameMap[row][col] = new Water(row, col);
             }
         }
     }
